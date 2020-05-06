5ea0cae71b9f87e719e56ec923c2b0ba9a80f79c
IngvarListard <vlzvoice@ya.ru>
2020-05-06 23:31:04 +0400

    chore: simplify main package

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"github.com/IngvarListard/courses-telebot/internal/db/models"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 // migrateSchema синхронизация сземы БД
 func migrateSchema() {
-	Db.AutoMigrate(&models.User{})
+	conf.Db.AutoMigrate(&models.User{})
 }
 
 func main() {
-	Setup()
-	defer Db.Close()
+	defer func() {
+		if err := conf.Db.Close(); err != nil {
+			fmt.Printf("Ошибка при закрытии соединения с БД: %v", err)
+		}
+	}()
 	migrateSchema()
 	Start()
 }
