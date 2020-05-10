1fcfcc94dafad47aa4801d1d3dece13cdf7bf499
IngvarListard <vlzvoice@ya.ru>
2020-05-10 20:15:14 +0400

    feat: create user and related chat record in database

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"fmt"
 	"github.com/IngvarListard/courses-telebot/internal/db/models"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/jinzhu/gorm"
@@ -13,36 +12,40 @@ var (
 )
 
 // Database connection setup
-func Setup() (DB *gorm.DB, err error) {
+func Setup() (*gorm.DB, error) {
+	var err error
 	DB, err = gorm.Open("sqlite3", "courses_bot.db")
 	if err != nil {
 		log.Printf("failed to connect database: %v", err)
 		return nil, err
 	}
-	MigrateSchema(DB)
-	//DB.AutoMigrate(&models.Chat{}, &models.User{}, &models.LearningNode{}, &models.Document{})
+	MigrateSchema()
 	return DB, err
 }
 
 // MigrateSchema database schema synchronization
-func MigrateSchema(DB *gorm.DB) {
+func MigrateSchema() {
 	DB.AutoMigrate(&models.Chat{}, &models.User{}, &models.LearningNode{}, &models.Document{})
 }
 
 func AddConversation(user *tgbotapi.User, chat *tgbotapi.Chat) {
-	c := DB.FirstOrCreate(&models.Chat{ID: chat.ID}, models.Chat{
+	newChat := DB.FirstOrCreate(&models.Chat{ID: chat.ID}, models.Chat{
 		ID:    chat.ID,
 		Type:  chat.Type,
 		Title: chat.Title,
 	})
-	fmt.Printf("AAAAA: %v", c.Value)
 
-	DB.FirstOrCreate(&models.User{ID: user.ID}, models.User{
+	chatVal := newChat.Value.(*models.Chat)
+	newUser := models.User{
 		ID:           user.ID,
 		FirstName:    user.FirstName,
 		LastName:     user.LastName,
 		UserName:     user.UserName,
 		LanguageCode: user.LanguageCode,
 		IsBot:        user.IsBot,
-	})
+	}
+	if chatVal != nil {
+		newUser.ChatID = chatVal.ID
+	}
+	DB.FirstOrCreate(&models.User{ID: user.ID}, newUser)
 }
