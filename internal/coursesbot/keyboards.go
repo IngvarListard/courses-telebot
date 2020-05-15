13de6b153dc8e067babc9bce7cb08ba3f45199e0
IngvarListard <vlzvoice@ya.ru>
2020-05-15 18:47:36 +0400

    chore: added emoji to keyboard

--- internal/coursesbot/keyboards.go
+++ internal/coursesbot/keyboards.go
@@ -13,12 +13,12 @@ func genCoursesKeyboard(nodes []*models.LearningNode, documents []*models.Docume
 	// TODO: pagination, navigation arrows, реализовать функцию обхода через интерфейс вместо двух циклов
 	for _, v := range nodes {
 		callback := fmt.Sprintf("getNodeList:%v", v.ID)
-		newRow := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(v.Name, callback))
+		newRow := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("\xF0\x9F\x93\x81 "+v.Name, callback))
 		rows = append(rows, newRow)
 	}
 	for _, v := range documents {
 		callback := fmt.Sprintf("getDocument:%v", v.ID)
-		newRow := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(v.Name, callback))
+		newRow := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("\xF0\x9F\x94\x8A"+v.Name, callback))
 		rows = append(rows, newRow)
 	}
 	keyboard := tgbotapi.NewInlineKeyboardMarkup(rows...)
