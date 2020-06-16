b5dd69005d5390662ac6c616c5e3c0b1b7273687
IngvarListard <vlzvoice@ya.ru>
2020-06-17 02:06:43 +0300

    feature: add document icon

--- internal/coursesbot/keyboards.go
+++ internal/coursesbot/keyboards.go
@@ -4,21 +4,31 @@ import (
 	"fmt"
 	"github.com/IngvarListard/courses-telebot/internal/models"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"strings"
 )
+
+var icons = map[string]string{
+	"dir":   "\xF0\x9F\x93\x81 ",
+	"audio": "\xF0\x9F\x94\x8A ",
+	"doc":   "\xF0\x9F\x93\x84",
+}
 
 func genCoursesKeyboard(nodes []*models.LearningNode, documents []*models.Document) (*tgbotapi.InlineKeyboardMarkup, error) {
 
 	var rows [][]tgbotapi.InlineKeyboardButton
 
-	// TODO: pagination, navigation arrows, реализовать функцию обхода через интерфейс вместо двух циклов
 	for _, v := range nodes {
 		callback := fmt.Sprintf("sendNodeList:%v", v.ID)
-		newRow := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("\xF0\x9F\x93\x81 "+v.Name, callback))
+		newRow := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(icons["dir"]+v.Name, callback))
 		rows = append(rows, newRow)
 	}
 	for _, v := range documents {
+		icon := icons["doc"]
+		if strings.HasSuffix(v.FileName, ".mp3") {
+			icon = icons["audio"]
+		}
 		callback := fmt.Sprintf("sendDocument:%v", v.ID)
-		newRow := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData("\xF0\x9F\x94\x8A"+v.Name, callback))
+		newRow := tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(icon+v.Name, callback))
 		rows = append(rows, newRow)
 	}
 	keyboard := tgbotapi.NewInlineKeyboardMarkup(rows...)
